bbc68121949a34c15549d7a16a36fb6b3a5348d8
Kungairola <dorjegairola@gmail.com>
2024-11-28 18:41:03 +0530

    i updated it again to solve the error

--- blog/app/page.tsx
+++ blog/app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
         My Portfolio
       </h1>
       <p className="mb-4">
-        {`Welcome to my corner of the world! I'm a mountain enthusiast with a passion for exploring high passes, learning from indigenous cultures, and unraveling the secrets of ecology. Currently, I'm delving into the fascinating realm of molecular ecology, while also learning to program in Python. When I'm not geeking out over science, you can find me dreaming of my next adventure in breathtaking destinations like Iceland, Norway, Denmark, Italy, Yukon, the Andes, Greenland, and Japan. Join me on this journey as I share tales from the mountains, musings on ecology, and insights into the intersection of technology and nature.`}
+        {`Welcome to my corner of the world! I'm a mountain enthusiast with a passion for exploring high passes, learning from indigenous cultures, and unraveling the secrets of ecology. Currently, I'm delving into the fascinating realm of molecular ecology, while also learning to program in Python. When I'm not geeking out over science, you can find me dreaming of my next adventure in breathtaking destinations like Iceland, Norway, Denmark, Italy, Yukon, the Andes, Greenland, and Japan. Join me on this journey as I share tales from the mountains, musings on ecology and insights into the intersection of technology and nature.`}
       </p>
       <div className="my-8">
         <BlogPosts />
